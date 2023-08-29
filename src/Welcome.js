e65f83fa06044feca863f969cbc35503593fbc8f
dina <timcud6@gmail.com>
2023-08-29 19:28:15 +0300

    added ex

---
+++ src/Welcome.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+  render() {
+    const { name, age } = this.props;
+
+    return (
+      <div>
+        <p>Welcome, {name}!</p>
+        <p>Your age is {age}.</p>
+      </div>
+    );
+  }
+}
+
+Welcome.defaultProps = {
+  name: "Guest",
+  age: null,
+};
+
+export default Welcome;
