6739fee3ec98bb58f6567cb4f750ed1db557bd5d
dina <timcud6@gmail.com>
2023-08-30 13:29:33 +0300

    added ex

---
+++ src/Welcome/Age.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Age(props) {
+  return <p>Your age is {props.age}.</p>;
+}
+
+export default Age;
---
+++ src/Welcome/Welcome.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Age from './Age';
+
+function Welcome(props) {
+  const { name, age } = props;
+
+  const isAgeInRange = age > 18 && age < 65;
+
+  return (
+    <div>
+      <p>Welcome, {name}!</p>
+      {isAgeInRange && <Age age={age} />}
+    </div>
+  );
+}
+
+Welcome.defaultProps = {
+  name: <strong>Guest</strong>,
+  age: null
+};
+
+export default Welcome;
