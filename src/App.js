2676eab3a530d7ce1a329836c0e46c53db6057a1
dina <timcud6@gmail.com>
2023-08-30 19:05:54 +0300

    added file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
+import { LanguageProvider } from "./LanguageContext /LanguageContext";
+import DisplayLanguage from './LanguageContext /DisplayLanguage'
+import LanguageSelector from "./LanguageContext /LanguageSelector";
 
 function App() {
   return (
     <div className="App">
-      app
+    <LanguageProvider>
+      <div>
+        <h1>Language App</h1>
+        <DisplayLanguage />
+        <LanguageSelector/>
+      </div>
+    </LanguageProvider>
     </div>
   );
 }
