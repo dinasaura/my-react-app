33311dddb0cb84108745c1f76aff320ff9cff6ae
dina <timcud6@gmail.com>
2023-08-30 13:45:54 +0300

    added ex

---
+++ src/Counter.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(prevState => ({
+        count: prevState.count + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <h1>{this.state.count}</h1>;
+  }
+}
+
+export default Counter;
+
