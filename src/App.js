9a955a104ba9eec994473fcff13e3d0cac7cb516
dina <timcud6@gmail.com>
2023-08-30 23:49:27 +0300

    added ex

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import React from 'react';
+import FilteredList from './FilteredList';
 
 function App() {
+  const itemList = [
+    { id: 1, name: 'Alice', age: 25 },
+    { id: 2, name: 'Luca', age: 17 },
+    { id: 3, name: 'John', age: 30 },
+  ];
+
   return (
-    <div className="App">
-      app
+    <div>
+      <FilteredList list={itemList} />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/FilteredList.js
@@ -0,0 +1,22 @@
+import React, { useMemo } from 'react';
+
+function FilteredList({ list }) {
+  const filteredList = useMemo(() => {
+    return list.filter(item => item.age > 18);
+  }, [list]);
+
+  return (
+    <div>
+      <h2>Filtered List</h2>
+      <ul>
+        {filteredList.map(item => (
+          <li key={item.id}>
+            {item.name} (Age: {item.age})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default FilteredList;
