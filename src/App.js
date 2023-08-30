b8613842f55ca578dd8e963bdb67188a950da853
dina <timcud6@gmail.com>
2023-08-30 15:34:52 +0300

    added file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Welcome from '../src copy/Welcome';
+import Welcome from './Welcome/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Welcome name={Alice} age={30} />
+      <Welcome name='Alice' age={30} />
     </div>
   );
 }
