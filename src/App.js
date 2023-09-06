ff7fe2aa89707c327480a6f33e95f3ca9478abd4
dina <timcud6@gmail.com>
2023-09-06 10:09:03 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Counter  from "./Counter";
+import Counter from "./Counter";
 import ShowGithubUser from "./GithubUsers/ShowGithubUsers";
 
+function App() {
+  const navigate = useNavigate();
 
- function App() {
-    const navigate = useNavigate()
+  function buttonNavigate() {
+    navigate("./Counter");
+  }
 
-    function buttonNavigate() {
-        navigate('./Counter')
-    }
-
-    return (
-        <div>
-            <button onClick={buttonNavigate}>Enter the counter app</button>
-             <Routes>
-                <Route path="/Counter" element={<Counter initialValue={0}/>} />
-                <Route path="/users/:username" element={<ShowGithubUser />} /> 
-            </Routes> 
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={buttonNavigate}>Entra nell'app del contatore</button>
+      <Routes>
+        <Route path="/Counter" element={<Counter initialValue={0} />} />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
+
 
