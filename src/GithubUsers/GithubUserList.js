7781fbfb45893a06a0b2e8b3ef63e0d7e6c2a39a
dina <timcud6@gmail.com>
2023-09-07 20:38:40 +0300

    added file

--- src/GithubUsers/GithubUserList.js
+++ src/GithubUsers/GithubUserList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Link, Routes } from "react-router-dom";
-import ShowGithubUsers from './ShowGithubUsers'
+import ShowGithubUser from './ShowGithubUsers'
 
 function GithubUserList() {
-  const usernames = ["user1", "user2", "user3"];
+  const usernames = ["utente1", "utente2", "utente3"];
 
   return (
     <div>
-      <h1>GitHub Users List</h1>
+      <h1>Lista Utenti GitHub</h1>
       <ul>
         {usernames.map((username) => (
           <li key={username}>
@@ -15,10 +15,14 @@ function GithubUserList() {
           </li>
         ))}
       </ul>
-
       <Routes>
+      <Route
+        index
+        element={<p>Aggiungi un utente e selezionalo</p>}
+      />
+      
         {usernames.map((username) => (
-          <Route key={username} path={username} element={<ShowGithubUsers username={username} />} />
+          <Route key={username} path={username} element={<ShowGithubUser username={username} />} />
         ))}
       </Routes>
     </div>
@@ -26,3 +30,4 @@ function GithubUserList() {
 }
 
 export default GithubUserList;
+
