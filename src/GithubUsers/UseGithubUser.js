2fcc4d385da2cbafa6d28d76a184e74bc3b75494
dina <timcud6@gmail.com>
2023-09-07 20:46:46 +0300

    added file

--- src/GithubUsers/UseGithubUser.js
+++ src/GithubUsers/UseGithubUser.js
@@ -1,29 +1,20 @@
-import { useState } from 'react';
+import useSWR from 'swr';
 
-function UseGithubUser(username) {
-  const [userData, setUserData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const fetchUser = async (username) => {
-    setLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch user data');
-      }
-      const data = await response.json();
-      setUserData(data);
-      setLoading(false);
-    } catch (error) {
-      setError(error);
-      setLoading(false);
-    }
-  };
-
-  return { userData, loading, error, fetchUser };
+function fetchUser(username) {
+  return fetch(`https://api.github.com/users/${username}`).then((response) =>
+    response.json()
+  );
 }
 
-export default UseGithubUser;
+function useGithubUser(username) {
+  const { data, error } = useSWR(username ? `/users/${username}` : null, fetchUser);
+
+  return {
+    userData: data,
+    loading: !data && !error,
+    error: error,
+  };
+}
+
+export default useGithubUser;
+
